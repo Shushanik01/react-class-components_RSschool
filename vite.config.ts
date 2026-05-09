dce44a75b1bb592c490c89e87f2e67dbeb407507
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-05-09 15:22:29 +0400

    chore: setup test environemnt

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,27 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ command }) => ({
   plugins: [react()],
   base: command === 'build' ? '/react-class-components_RSschool/' : '/',
+  test: {
+  environment: 'jsdom',
+  globals: true,
+  setupFiles: './src/setupTests.ts',
+  coverage: {
+    provider: 'v8',
+    include: ['src/**/*.{js,jsx,ts,tsx}'],
+    exclude: [
+      'src/**/*.test.{js,jsx,ts,tsx}',
+      'src/**/*.spec.{js,jsx,ts,tsx}',
+      'src/index.{js,jsx,ts,tsx}',
+      'src/setupTests.{js,ts}',
+      'src/**/*.d.ts',
+    ],
+    thresholds: {
+      statements: 80,
+      branches: 50,
+      functions: 50,
+      lines: 50,
+    },
+  },
+},
+
 }));
