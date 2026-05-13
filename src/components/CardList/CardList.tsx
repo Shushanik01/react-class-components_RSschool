a10d16bcda2336ba082ccd34ba550b517f391f40
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-05-14 00:51:37 +0400

    chore: convert LoadingSpinner to functional component

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -2,23 +2,22 @@ import type { CardListProps } from '../../types';
 import styles from './CardList.module.css';
 import CardItem from '../CardItem/CardItem';
 
-
-const CardList = (props: CardListProps)=>{
-  return(
+const CardList = (props: CardListProps) => {
+  return (
     <ul className={styles.cardListContainer}>
-        {(props.items ?? []).length === 0 && <p>No results found</p>}
-        {(props.items ?? []).map((item) => (
-          <li key={item.name}>
-            <CardItem
-              name={item.name}
-              type={item.types[0].type.name}
-              weight={item.weight}
-              ability={item.abilities[0].ability.name}
-              image={item.sprites.front_default}
-            />
-          </li>
-        ))}
-      </ul>
-  )
-}
-export default CardList
+      {(props.items ?? []).length === 0 && <p>No results found</p>}
+      {(props.items ?? []).map((item) => (
+        <li key={item.name}>
+          <CardItem
+            name={item.name}
+            type={item.types[0].type.name}
+            weight={item.weight}
+            ability={item.abilities[0].ability.name}
+            image={item.sprites.front_default}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default CardList;
--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,14 +1,13 @@
-import { Component } from 'react';
 import styles from './LoadingSpinner.module.css';
 
-class LoadingSpinner extends Component {
-  render() {
+const LoadingSpinner = ()=> {
+  
     return (
       <div className={styles.wrapper} role="status" aria-label="Loading">
         <div className={styles.spinner}></div>
       </div>
     );
-  }
+  
 }
 
 export default LoadingSpinner;
